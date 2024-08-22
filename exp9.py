000edcbdd4c37535e4adf05d486230c92ec0f62c
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-08-22 12:59:08 +0530

    Update exp9.py

--- exp9.py
+++ exp9.py
@@ -2,6 +2,8 @@ import numpy as np
 
 arr = np.array([1,2,3])
 
+# Trogonometric Functions
+'''
 print(arr)  # Output: [1 2 3]
 
 print(np.sin(arr)) # Output : [0.84147098 0.90929743 0.14112001]
@@ -13,3 +15,12 @@ print(np.tan(arr)) # Output : [ 1.55740772 -2.18503986 -0.14254654]
 print(np.radians(30)) # Output : 0.52359878
 
 print(np.degrees(0.52359878)) # Output : 30.00000025
+'''
+
+# Arithmatic Functions
+
+print(np.add(3,6)) # Output : 9
+
+print(np.subtract(9,6)) # Output : 3
+
+print(np.multiply(9,6)) # Output : 54
