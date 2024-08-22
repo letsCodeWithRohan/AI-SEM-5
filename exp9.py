1ca90437048d609ec2f8abc0d2abbd9cde955a21
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-08-22 12:28:01 +0530

    Update exp9.py

--- exp9.py
+++ exp9.py
@@ -1,3 +1,15 @@
 import numpy as np
 
-np.
+arr = np.array([1,2,3])
+
+print(arr)  # Output: [1 2 3]
+
+print(np.sin(arr)) # Output : [0.84147098 0.90929743 0.14112001]
+
+print(np.cos(arr)) # Output : [ 0.54030231 -0.41614684 -0.9899925 ]
+
+print(np.tan(arr)) # Output : [ 1.55740772 -2.18503986 -0.14254654]
+
+print(np.radians(30)) # Output : 0.52359878
+
+print(np.degrees(0.52359878)) # Output : 30.00000025
