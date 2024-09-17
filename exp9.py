2317aca6fb4e83f5e27b7ad0c706361121bcacfc
Chaudhari Rohan <134542326+letsCodeWithRohan@users.noreply.github.com>
2024-09-17 13:16:37 +0530

    Update exp9.py

--- exp9.py
+++ exp9.py
@@ -1,20 +1,19 @@
 import numpy as np
 
-arr = np.array([1,2,3])
+arr = np.array([30,45,60])
 
 # Trogonometric Functions
 '''
-print(arr)  # Output: [1 2 3]
 
-print(np.sin(arr)) # Output : [0.84147098 0.90929743 0.14112001]
+print("Sin Values : ",np.sin(arr))
 
-print(np.cos(arr)) # Output : [ 0.54030231 -0.41614684 -0.9899925 ]
+print("Cos Values : ",np.cos(arr))
 
-print(np.tan(arr)) # Output : [ 1.55740772 -2.18503986 -0.14254654]
+print("Tan Values : ",np.tan(arr))
 
-print(np.radians(30)) # Output : 0.52359878
+print("Degree to Radian : ",np.radians(30))
 
-print(np.degrees(0.52359878)) # Output : 30.00000025
+print("Radian to Degree : ",np.degrees(0.52359878))
 '''
 
 # Arithmatic Functions
